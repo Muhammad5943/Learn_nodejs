2082923b25eb992365d573e2e553c19b85120722
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-09-16 19:34:11 +0700

    this my trining for Nodejs Expressjs CRUD

--- server.js
+++ server.js
@@ -7,20 +7,17 @@ const bodyparser = require('body-parser');
 
 const employeeController = require('./controllers/employeeController');
 
-const app = express();
-const port = process.env.PORT || 3000
-
+var app = express();
 app.use(bodyparser.urlencoded({
     extended: true
 }));
 app.use(bodyparser.json());
-app.set('views',path.join(__dirname,'/views/'));
+app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine','hbs');
-
+app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
-    console.log(`Listening to localhost:${port}`);
+    console.log('Express server started at port : 3000');
 });
 
 app.use('/employee', employeeController);
